b2157d203e3aa3dec2a4d9e7eb3779b48079d64f
Vedant Kumar <vk@vedantk.com>
2012-11-01 05:41:42 -0700

    Adds some code to deal with constants.

--- diagonalize.cc
+++ diagonalize.cc
@@ -29,10 +29,11 @@ struct ADPass : public LoopPass
     DominatorTree* DT;
     std::vector<BasicBlock*> blocks;
 
-    /* Keep track of the loop iterator. */
-    int iter_base;
+    /* Track the loop range, iterator, and transformation matrix. */
+    int64_t iter_base;
     Value* iter_final;
     PHINode* iter_var;
+    MatrixXcd TransformationMatrix;
 
     static char ID;
 
@@ -40,38 +41,6 @@ struct ADPass : public LoopPass
 
     virtual void getAnalysisUsage(AnalysisUsage& AU) const {
         AU.addRequired<DominatorTree>();
-    }
-
-    bool extractIterator(ICmpInst* icmp) {
-        /* In canonical form, LLVM should pass us an ICmp with the predicate
-         * reduced to an equality comparison. The LHS should contain the loop
-         * increment, and the RHS should be an out-of-loop 'constant'. */
-        Value *cmpLhs, *cmpRhs;
-        ICmpInst::Predicate IPred; 
-        if (match(icmp, m_ICmp(IPred, m_Value(cmpLhs),
-                                      m_Value(cmpRhs)))
-            && IPred == CmpInst::Predicate::ICMP_EQ)
-        {
-            if (isa<Instruction>(cmpRhs)) {
-                Instruction* irhs = cast<Instruction>(cmpRhs);
-                if (!DT->properlyDominates(irhs->getParent(), blocks.front())) {
-                    return false;
-                }
-            }
-
-            Value *incrLhs, *incrRhs;
-            if (match(cmpLhs, m_Add(m_Value(incrLhs), m_Value(incrRhs)))) {
-                if (isa<PHINode>(incrLhs)
-                    && isa<ConstantInt>(incrRhs)
-                    && cast<ConstantInt>(incrRhs)->isOne())
-                {
-                    iter_var = cast<PHINode>(incrLhs);
-                    iter_final = cmpRhs;
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     virtual bool runOnLoop(Loop* L, LPPassManager&) {
@@ -87,7 +56,7 @@ struct ADPass : public LoopPass
         blocks = loop->getBlocks();
         DT = &getAnalysis<DominatorTree>();
         
-        /* Filter away loops with invalid instructions. */
+        /* Filter away loops with unsupported instructions. */
         int nr_cmps = 0;
         ICmpInst* icmp = NULL;
         SmallPtrSet<PHINode*, 4> phis;
@@ -130,26 +99,101 @@ struct ADPass : public LoopPass
             }
         }
 
-        /* Extract the induction variable if possible. */
-        if (!extractIterator(icmp)) {
+        /* Extract the loop iterator if possible. */
+        if (!icmp || !extractIterator(icmp)) {
             return false;
         } else {
             phis.erase(iter_var);
         }
         
-        /* XXX:
-         * Iterate over the PHI nodes, examining their update values from the
-         * bottom-up. Ensure there are no non-linear dependencies. N.B: the values
-         * we see from the bottom-up are the 'old' versions of the state variables.
-         */
-        MatrixXcd(phis.size(), phis.size());
+        /* Find the initial state and all linear dependence relations. */
+        int count = 0;
+        MatrixXd InitialState = MatrixXd(phis.size(), 1);
+        TransformationMatrix = MatrixXcd(phis.size(), phis.size());
         for (auto it = phis.begin(); it != phis.end(); ++it) {
-            errs() << **it << "\n";
-            
-            
+            PHINode* PN = *it;
+            try {
+                InitialState(count, 0) = DoubleFromValue(PN->getIncomingValue(0));
+            } catch (int) {
+                return false;
+            }
+            if (!trackUpdates(count, PN)) {
+                return false;
+            }
+            ++count;
         }
+
+        /* XXX:
+         * How do we check if complex eigenvalues were generated? Avoid this case...
+         */
+        EigenSolver<MatrixXd> EigGen(TransformationMatrix);
+        MatrixXcd P = EigGen.eigenvectors();
+        MatrixXcd D = EigGen.eigenvalues().asDiagonal();
+        MatrixXcd Pinv = P.inverse();
+
+        /* XXX:
+         * Perform P * (D^[iter_final]) * Pinv * InitialState, and assign the final
+         * values back into the state variables.
+         */
         
+        /* XXX:
+         * return true;
+         */
         return false;
+    }
+
+    bool extractIterator(ICmpInst* icmp) {
+        /* In canonical form, LLVM should pass us an ICmp with the predicate
+         * reduced to an equality comparison. The LHS should contain the loop
+         * increment, and the RHS should be an out-of-loop 'constant'. */
+        Value *cmpLhs, *cmpRhs;
+        ICmpInst::Predicate IPred; 
+        if (match(icmp, m_ICmp(IPred, m_Value(cmpLhs),
+                                      m_Value(cmpRhs)))
+            && IPred == CmpInst::Predicate::ICMP_EQ)
+        {
+            if (isa<Instruction>(cmpRhs)) {
+                Instruction* irhs = cast<Instruction>(cmpRhs);
+                if (!DT->properlyDominates(irhs->getParent(), blocks.front())) {
+                    return false;
+                }
+            }
+
+            Value *incrLhs, *incrRhs;
+            if (match(cmpLhs, m_Add(m_Value(incrLhs), m_Value(incrRhs)))) {
+                if (isa<PHINode>(incrLhs)
+                    && isa<ConstantInt>(incrRhs)
+                    && cast<ConstantInt>(incrRhs)->isOne())
+                {
+                    iter_final = cmpRhs;
+                    iter_var = cast<PHINode>(incrLhs);
+                    Value* iter_initial = iter_var->getIncomingValue(0);
+                    if (isa<ConstantInt>(iter_initial)) {
+                        iter_base = IntFromValue(iter_initial);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    bool trackUpdates(int count, PHINode* PN) {
+        return false;
+    }
+
+    int64_t IntFromValue(Value* V) {
+        return cast<ConstantInt>(V)->getValue().getSExtValue();
+    }
+
+    double DoubleFromValue(Value* V) {
+        if (isa<ConstantInt>(V)) {
+            return double(IntFromValue(V));
+        } else if (isa<ConstantFP>(V)) {
+            return cast<ConstantFP>(V)->getValueAPF().convertToDouble();
+        } else {
+            throw 0;
+        }
     }
 };
 
